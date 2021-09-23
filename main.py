341a4350d792c43240cf881b1f6fb2e702b402a7
Agustin Rojas <rojasagustin90@gmail.com>
2021-09-22 22:52:18 -0300

    init step 1

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+import uuid
+
 import uvicorn
 from fastapi import FastAPI
+from pydantic.main import BaseModel
 
 app = FastAPI()
 
+
+class UserRequest(BaseModel):
+    username: str
+
+
+class User:
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+
+
 users = []
+
 
 @app.get('/users')
 async def get_users():
     return users
 
+
+@app.post('/users')
+async def create_users(user_request: UserRequest):
+    new_user = User(id=str(uuid.uuid4()), username=user_request.username)
+    users.append(new_user)
+    return new_user
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=5000)
